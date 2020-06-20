06bc096baab93c4cc7efb14ef8309d87a62b185f
Artur Żywczak <webox90804@gmail.com>
2020-06-20 12:11:01 +0200

    Add project files.

---
+++ Zajecia9/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zajecia9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StudentView view = new StudentView();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            this.DataContext = view;
+            listBox1.ItemsSource = view.List.Students;
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            tbId.Text = "";
+            tbName.Text = "";
+            tbSurname.Text = "";
+            cBirthday.SelectedDate = null;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            
+            view.List.Students.Add(new Student
+            {
+                id = int.Parse(tbId.Text),
+                name = tbName.Text,
+                surName = tbSurname.Text,
+                birthday = (DateTime)cBirthday.SelectedDate
+            }) ;
+            
+
+            //view = new StudentView();
+            this.DataContext = view;
+            listBox1.ItemsSource = view.List.Students;
+
+            btnReset_Click(sender, e);
+        }
+    }
+}
---
+++ Zajecia9/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zajecia9
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public string surName { get; set; }
+        public DateTime birthday { get; set; }
+    }
+
+    public class StudentList
+    {
+        public ObservableCollection<Student> Students { get; set; }
+    }
+
+    public class StudentView
+    {
+        public StudentList List { get; set; }
+
+        public StudentView()
+        {
+            List = new StudentList()
+            {
+                Students = new ObservableCollection<Student>()
+                {
+                    new Student
+                    {
+                    id = 1,
+                    name = "Adam",
+                    surName = "Abacki",
+                    birthday = new DateTime(1996, 04, 10),
+                    },
+                    new Student
+                    {
+                    id = 2,
+                    name = "Barbara",
+                    surName = "Babacka",
+                    birthday = new DateTime(1966, 06, 16),
+                    },
+                    new Student
+                    {
+                    id = 3,
+                    name = "Cyprian",
+                    surName = "Cabacki",
+                    birthday = new DateTime(2002, 12, 12),
+                    },
+                    new Student
+                    {
+                    id = 4,
+                    name = "Dawid",
+                    surName = "Dabacki",
+                    birthday = new DateTime(2012, 10, 10),
+                    }
+
+                }
+            };
+        }
+    }
+
+
+}
